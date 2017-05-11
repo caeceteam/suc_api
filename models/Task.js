980473eba97024d5b21326a0c9fee7de9d4d5e72
Ezequiel Fridman <ezequiel.fridman@mercadolibre.com>
2017-05-11 18:45:16 -0300

    subo cosas raras

--- models/Task.js
+++ models/Task.js
@@ -1,25 +1,22 @@
-var db=require('../dbconnection'); //reference of dbconnection.js
- 
-var Task={
- 
-getAllTasks:function(callback){
- 
-return db.query("Select * from task",callback);
- 
-},
- getTaskById:function(id,callback){
- 
-return db.query("select * from task where Id=?",[id],callback);
- },
- addTask:function(Task,callback){
- return db.query("Insert into task values(?,?,?)",[Task.Id,Task.Title,Task.Status],callback);
- },
- deleteTask:function(id,callback){
-  return db.query("delete from task where Id=?",[id],callback);
- },
- updateTask:function(id,Task,callback){
-  return db.query("update task set Title=?,Status=? where Id=?",[Task.Title,Task.Status,id],callback);
- }
- 
+/* jshint indent: 2 */
+"use strict";
+
+module.exports = function(sequelize, DataTypes) {
+  return sequelize.define('task', {
+    Id: {
+      type: DataTypes.STRING(50),
+      allowNull: false,
+      primaryKey: true
+    },
+    Title: {
+      type: DataTypes.STRING(500),
+      allowNull: true
+    },
+    Status: {
+      type: DataTypes.STRING(100),
+      allowNull: true
+    }
+  }, {
+    tableName: 'task'
+  });
 };
- module.exports=Task;
--- routes/Tasks.js
+++ routes/Tasks.js
@@ -1,77 +1,40 @@
 var express = require('express');
  var router = express.Router();
- var Task=require('../models/Task');
+ var models=require('../models/');
  
 router.get('/:id?',function(req,res,next){
  
-if(req.params.id){
- 
-Task.getTaskById(req.params.id,function(err,rows){
- 
-if(err)
-  {
-  res.json(err);
-  }
-  else{
-  res.json(rows);
-  }
-  });
- }
- else{
- 
-Task.getAllTasks(function(err,rows){
- 
-if(err)
-  {
-  res.json(err);
-  }
-  else
-  {
-  res.json(rows);
-  }
- 
- });
- }
- });
- router.post('/',function(req,res,next){
- 
-Task.addTask(req.body,function(err,count){
-  if(err)
-  {
-  res.json(err);
-  }
-  else{
-  res.json(req.body);//or return count for 1 &amp;amp;amp; 0
-  }
-  });
- });
- router.delete('/:id',function(req,res,next){
- 
-Task.deleteTask(req.params.id,function(err,count){
- 
-if(err)
-  {
-  res.json(err);
-  }
-  else
-  {
-  res.json(count);
-  }
- 
+    if(req.params.id){
+     
+        models.task.findAll({where:{id:req.params.id}}).then(function(tasks){
+          res.json(tasks);
+        });
+     }else{
+        models.task.findAll().then(function(tasks){
+        res.json(tasks);
+      });
+    }
 });
- });
- router.put('/:id',function(req,res,next){
- 
-Task.updateTask(req.params.id,req.body,function(err,rows){
- 
-if(err)
-  {
-  res.json(err);
-  }
-  else
-  {
-  res.json(rows);
-  }
+
+router.post('/', function(req,res,next){
+  console.log(req.body.id);
+  models.task.create({Id:req.body.id, Title: req.body.title, Status:req.body.status}).then(function(task){
+    res.json(task);
   });
- });
+});
+
+router.put('/:id', function(req,res,next){
+  models.task.find({where:{Id:req.params.id}}).then(function(task){
+    console.log("se encontro la tarea");
+    console.log(task);
+    task.Title = req.body.title;
+    task.Status = req.body.status;
+    task.save();
+    console.log("se hizo update");
+    console.log(task);
+    res.json(task);
+  });
+
+});
+ 
  module.exports=router;
