9aefcbdb4fe695df83c228fe8f4f927d27e20ed8
Ezequiel Fridman <ezequiel.fridman@mercadolibre.com>
2017-05-17 21:08:11 -0300

    agrego conf de BD + modelo de User

---
+++ models/User.js
@@ -0,0 +1,59 @@
+/* jshint indent: 2 */
+
+module.exports = function(sequelize, DataTypes) {
+  return sequelize.define('User', {
+    idUser: {
+      type: DataTypes.BIGINT,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    name: {
+      type: DataTypes.STRING(45),
+      allowNull: false
+    },
+    surname: {
+      type: DataTypes.STRING(45),
+      allowNull: false
+    },
+    alias: {
+      type: DataTypes.STRING(12),
+      allowNull: false,
+      unique: true
+    },
+    pass: {
+      type: DataTypes.CHAR(64),
+      allowNull: false
+    },
+    mail: {
+      type: DataTypes.STRING(60),
+      allowNull: false
+    },
+    idDiner: {
+      type: DataTypes.BIGINT,
+      allowNull: false
+    },
+    phone: {
+      type: DataTypes.STRING(20),
+      allowNull: true
+    },
+    state: {
+      type: DataTypes.INTEGER(11),
+      allowNull: false
+    },
+    role: {
+      type: DataTypes.INTEGER(11),
+      allowNull: false
+    },
+    docNumber: {
+      type: DataTypes.STRING(20),
+      allowNull: true
+    },
+    bornDate: {
+      type: DataTypes.DATE,
+      allowNull: true
+    }
+  }, {
+    tableName: 'User'
+  });
+};
