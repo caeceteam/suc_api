4c39f7e82267fd9c6af77a1addc2eba4fb729b72
Ezequiel Fridman <ezequiel.fridman@mercadolibre.com>
2017-05-27 13:43:30 -0300

    agrego validacion de usuario, alta y get de usuarios

---
+++ routes/authentication.js
@@ -0,0 +1,52 @@
+var express = require('express');
+var router = express.Router();
+var jwt = require('jwt-simple')
+var models = require('../models/');
+var app = express();
+var moment = require('moment');
+var expires = moment().add(7,'days').valueOf();
+var Sequelize = require('sequelize');
+const crypto = require('crypto');
+app.set('jwtTokenSecret', 'sucapi-2017_');
+
+/* Authenticate user and return token. */
+router.post('/', function(req, res, next) {
+  var userName = req.body.userName;
+  var password = req.body.password;
+  var hash = crypto.createHash('sha256');
+  var User = models.User;
+
+  User.find({where:Sequelize.or({alias:userName},{mail:userName})}).then(function(user,err){   
+   if (err) { 
+    // user not found 
+    return res.sendStatus(401);
+  }
+
+  if (!user) {
+    // incorrect username
+    return res.sendStatus(404);
+  }
+
+var hashedPass = hash.update(password).digest("base64");
+console.log(hashedPass);
+  if (user.pass !== hashedPass) {
+    // incorrect password
+    return res.sendStatus(401);
+  }
+  // User has authenticated OK
+  var token = jwt.encode({
+    iss: user.idUser,
+    exp: expires
+  }, app.get('jwtTokenSecret'));
+
+  res.json({
+    token : token,
+    expires: expires
+  });
+
+ });
+});
+
+
+
+module.exports = router;
