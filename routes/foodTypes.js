2a744f2549a945a53f94c54f16d5f04fcf33ed7e
Ezequiel Fridman <ezequiel.fridman@mercadolibre.com>
2017-07-09 18:41:51 -0300

    refactor en foodTypes

--- routes/foodTypes.js
+++ routes/foodTypes.js
@@ -1,108 +1,62 @@
 var express = require('express');
 var router = express.Router();
-var models = require('../models/');
 var app = express();
-var Sequelize = require('sequelize');
+var foodTypesService = require('../services/foodTypesService');
 
 /* GET foodTypes listing. */
 router.get('/:idFoodType?', function (req, res, next) {
     var idFoodType = req.params.idFoodType;
-    var foodTypes = models.FoodType;
     if (idFoodType) {
-        foodTypes.find({ where: { idFoodType: idFoodType } }).then(function (foodType, err) {
-            if (err) {
-                // foodType not found 
-                return res.status(401).json({});
+        foodTypesService.getFoodType(idFoodType, function (err, result) {
+            if (!err) {
+                res.status(result.status).json(result.body);
+            } else {
+                res.status(err.status).json(err.body);
             }
-
-            if (!foodType) {
-                // incorrect foodType
-                return res.status(404).json({});
-            }
-
-            res.json({
-                foodType: foodType.toJSON(),
-            });
         });
     } else {
-        var page_size = req.query.pageSize ? req.query.pageSize : 10;
-        var page = req.query.page ? req.query.page : 0;
-        var total_elements;
-        foodTypes.count().then(function(quantity){
-            total_elements = quantity;
-        });
-        foodTypes.findAll({ offset: page_size * page, limit: Math.ceil(page_size) }).then(function (foodTypesCol) {
-            var total_pages = Math.ceil(total_elements / page_size);
-            var number_of_elements = foodTypesCol.length;
-            res.json({
-                foodTypes: foodTypesCol,
-                pagination: {
-                    page: page,
-                    size: page_size,
-                    number_of_elements: number_of_elements,
-                    total_pages: total_pages,
-                    total_elements: total_elements
-                }
-            });
+        foodTypesService.getAllFoodTypes(req, function (err, result) {
+            if (!err) {
+                res.status(result.status).json(result.body);
+            } else {
+                res.status(err.status).json(err.body);
+            }
         });
     }
 });
 
 /* POST de foodTypes. */
 router.post('/', function (req, res, next) {
-    var foodTypes = models.FoodType;
-
-    var postFoodType = {
-        code: req.body.code,
-        name: req.body.name,
-        description: req.body.description,
-        perishable: req.body.perishable,
-        diabetic: req.body.diabetic,
-        celiac: req.body.celiac
-    }
-
-    foodTypes.create(postFoodType).then(function (foodType) {
-        res.status(201).json(foodType);
-    }).catch(error => {
-        res.status(500).json({ 'result': 'Ocurrio un error creando el foodType' });
+    var foodTypeRequest = req.body;
+    foodTypesService.createFoodType(foodTypeRequest, function (err, result) {
+        if (!err) {
+            res.status(result.status).json(result.body);
+        } else {
+            res.status(err.status).json(err.body);
+        }
     });
 });
 
 
 router.put('/:idFoodType', function (req, res, next) {
-    var foodTypes = models.FoodType;
     var idFoodType = req.params.idFoodType;
-    foodTypes.find({ where: { idFoodType: idFoodType } }).then(function (foodType) {
-        if (foodType) {
-            foodType.update({
-                code: req.body.code,
-                name: req.body.name,
-                description: req.body.description,
-                perishable: req.body.perishable,
-                diabetic: req.body.diabetic,
-                celiac: req.body.celiac
-            }).then(function (updatedFT) {
-                res.status(202).json(foodType);
-            }).catch(error => {
-                res.status(500).json({ 'result': 'Ha ocurrido un error actulizando el foodType ' + idFoodType });
-            });
+    foodTypesService.updateFoodType(idFoodType, req.body, function (err, result) {
+        if (!err) {
+            res.status(result.status).json(result.body);
         } else {
-            res.status(404).json({ 'result': 'No se encontro el foodType ' + idFoodType + ' para hacer el update' });
+            res.status(err.status).json(err.body);
         }
     });
 });
 
 router.delete('/:idFoodType', function (req, res, next) {
-    var foodTypes = models.FoodType;
     var idFoodType = req.params.idFoodType;
-    foodTypes.destroy({ where: { idFoodType: idFoodType } }).then(function (result) {
-        if (result == 1) {
-            res.status(200).json({ 'result': "Se ha borrado el foodType " + idFoodType });
+    foodTypesService.deleteFoodType(idFoodType, function (err, result) {
+        if (!err) {
+            res.status(result.status).json(result.body);
         } else {
-            res.status(204).json({ 'result': "No se ha podido borrar el foodType " + idFoodType });
+            res.status(err.status).json(err.body);
         }
-    }).catch(error => {
-        res.status(500).json({ 'result': 'Error eliminando el foodType ' + idFoodType });
     });
 });
 
