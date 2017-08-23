29e214c0c1ce53ea6419f060c13c058fb944f1b0
Ezequiel Fridman <ezequiel.fridman@mercadolibre.com>
2017-08-23 19:30:16 -0300

    fix para obtener diners por state

--- routes/enumerations.js
+++ routes/enumerations.js
@@ -1,25 +1,12 @@
 var express = require('express');
 var router = express.Router();
 var models = require('../models/');
+var enumerations = require('../enums/enumerations');
 var app = express();
-
-var enumerations = {
-    dinerStates: {
-        "pending": 0,
-        "approved": 1,
-        "rejected": 2
-    },
-    userActive: {
-        "false": 0,
-        "true": 1
-    }
-};
-
-router.enumerations = enumerations;
 
 /* GET enumerations listing. */
 router.get('/', function (req, res, next) {
-    return res.json(enumerations);
+    return res.json(enumerations.enums);
 });
 
 
