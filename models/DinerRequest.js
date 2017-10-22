4563744ae247e841f9d788bb75b38c99ac2560b8
Ezequiel Fridman <ezequiel.fridman@mercadolibre.com>
2017-10-22 19:44:43 -0300

    fix in status definition for diner request

--- models/DinerRequest.js
+++ models/DinerRequest.js
@@ -29,7 +29,7 @@ module.exports = function(sequelize, DataTypes) {
       allowNull: false
     },
     status: {
-      type: DataTypes.INTEGER(3),
+      type: DataTypes.INTEGER(2),
       allowNull: false
     }
   }, {
