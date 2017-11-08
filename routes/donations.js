4ac047e7122256a061eabf23ddcbf4db3534eff4
Ezequiel Fridman <ezequiel.fridman@mercadolibre.com>
2017-11-08 19:23:27 -0300

    saco un log

--- routes/donations.js
+++ routes/donations.js
@@ -53,7 +53,6 @@ router.post('/', function (req, res, next) {
     var donationRequest = req.body;
     donationsService.createDonation(donationRequest, function (err, result) {
         if (!err) {
-            console.log(result);
             res.status(result.status).json(result.body);
         } else {
             res.status(err.status).json(err.body);
