b976e58d1f1d435f06f21cdff98ff2570b50b167
Ezequiel Fridman <ezequiel.fridman@mercadolibre.com>
2017-11-26 02:16:32 -0300

    arreglo en manejo de error con mail

--- services/emailService.js
+++ services/emailService.js
@@ -22,7 +22,7 @@ var options = {
   extName: '.hbs'
 };
 
-var transport = nodemailer.createTransport("SMTP", {
+var transport = nodemailer.createTransport({
   service: "Gmail",
   auth: {
     XOAuth2: {
